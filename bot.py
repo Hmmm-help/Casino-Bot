92b0d0ec8b5aa6553d6449057f3d0ae204502c18
acid_utopia <g.aaliyah1016@gmail.com>
2020-03-10 15:27:21 +0000

    Coin_Flip

--- bot.py
+++ bot.py
@@ -8,3 +8,7 @@ token = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='$')
 
+@bot.command(name= "Coin-Flip")
+async def Coin_Flip(ctx):
+    import random
+    await (random.choice(["HEADS", "Tails"]))
