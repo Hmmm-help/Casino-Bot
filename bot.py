1c53a89b08d4f1e7b2251aa6882a10dc9af1f6e7
Hmmm-help <60788402+Hmmm-help@users.noreply.github.com>
2020-03-12 11:25:14 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,6 +8,6 @@ bot = commands.Bot(command_prefix='$')
 @bot.command(name= "Coin-Flip")
 async def Coin_Flip(ctx):
     import random
-    await ctx.send(random.choice(["HEADS", "Tails"]))
+    await ctx.send(random.choice(["HEADS", "TAILS"]))
 
 bot.run(token)
