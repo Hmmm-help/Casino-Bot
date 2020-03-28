9e543bdbc9dbc386ed30d1660238b539c8451d62
acid_utopia <g.aaliyah1016@gmail.com>
2020-03-28 19:01:45 +0000

    Creation of Emoji Slot Machine

--- bot.py
+++ bot.py
@@ -9,3 +9,16 @@ bot = commands.Bot(command_prefix='$')
 async def Coin_Flip(ctx):
     import random
     await (random.choice(["HEADS", "Tails"]))
+
+@bot.command(name= "Emoji-Slot")
+async def emjoi_slot(ctx):
+    import random
+    options = ["🤡","😷","👽"]
+    slot = [random.choice(options),random.choice(options),random.choice(options)]
+    print(slot)
+    outcome = all(x == slot[0] for x in slot)
+    print(outcome)
+    if outcome == False:
+        await "Sorry son, this ain't it- ya lost some cash."
+    if outcome == True:
+        await "Look at you! Don't be shy- play some more!"
