929a6559e9ab7803ae3c39d0c246cffa8a57adcf
irisg648 <irisgonzalez648@gmail.com>
2020-03-31 14:43:56 +0000

    Added a counter command, revised Coin-Flip

--- bot.py
+++ bot.py
@@ -1,13 +1,25 @@
 import os
+import random
+
 from discord.ext import commands
 
 token = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='$')
 
+wins = 0
+losses = 0
+
 @bot.command(name= "Coin-Flip")
 async def Coin_Flip(ctx):
-    import random
-    await ctx.send(random.choice(["HEADS", "TAILS"]))
+    choice = random.choice(["HEADS", "TAILS"])
+    await ctx.send(choice)
+    if choice == "HEADS":
+        wins += 1
+    else:
+        losses +- 1
 
+@bot.command(name="counter")
+async def counter(ctx):
+    await ctx.send(f"Your score is now {wins}:{losses}.")
 bot.run(token)
