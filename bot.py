8366c0acf36cf6eb723043851f564fe8301d845e
acid_utopia <g.aaliyah1016@gmail.com>
2020-03-31 15:20:04 +0000

    added Emoji Slot

--- bot.py
+++ bot.py
@@ -10,4 +10,16 @@ async def Coin_Flip(ctx):
     import random
     await ctx.send(random.choice(["HEADS", "TAILS"]))
 
+@bot.command(name= "Emoji-Slot")
+async def emjoi_slot(ctx):
+    import random
+    options = ["🤡","😷","👽"]
+    slot = [random.choice(options),random.choice(options),random.choice(options)]
+    await ctx.send(slot)
+    outcome = all(x == slot[0] for x in slot)
+    if outcome == False:
+        await ctx.send("Sorry son, this ain't it- ya lost some cash.")
+    if outcome == True:
+        await ctx.send("Look at you! Don't be shy- play some more!")
+        
 bot.run(token)
