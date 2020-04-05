b51e1f0bfbcbc671ef17581f179af995d4e7d0b9
Hmmm-help <60788402+Hmmm-help@users.noreply.github.com>
2020-04-05 18:43:43 -0400

    Deletion of money system

--- bot.py
+++ bot.py
@@ -5,8 +5,6 @@ token = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='$')
 
-all_balances = {i : 100 for i in users.id}
-
 @bot.command(name= "Coin-Flip")
 async def Coin_Flip(ctx):
     import random
@@ -29,13 +27,10 @@ async def emjoi_slot(ctx):
     await ctx.send(slot)
     outcome = all(x == slot[0] for x in slot)
     if outcome == False:
-        await ctx.send(f"New Balance for {user.name} is {all_balances[user.id] - 10}")
         await ctx.send("Sorry son, this ain't it- ya lost some cash.")
-        all_balances[user.id] = all_balances[user.id] - 10
     if outcome == True:
-        await ctx.send(f"New Balance for {user.name} is {all_balances[user.id] + 10}")
         await ctx.send("Look at you! Don't be shy- play some more!")
-        all_balances[user.id] = all_balances[user.id] + 10
+
         
         
 bot.run(token)
