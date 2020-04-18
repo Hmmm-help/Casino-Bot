0f966900a05c7b30225cad045dcd1e6057712e70
acid_utopia <g.aaliyah1016@gmail.com>
2020-04-18 00:15:29 +0000

    Finally made a JSON file?

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 import os
+import json
 import random
 from discord.ext import commands
 
+os.chdir(r"/project/Hmmm-help-Casino-Bot")
 token = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='$')
@@ -9,12 +11,30 @@ bot = commands.Bot(command_prefix='$')
 wins = 0
 losses = 0  
 
+async def update_data(users, user):
+    if not user.id in users:
+        users[user.id] = {}
+        users[user.id]["balance"] = 100
 
-@bot.command(name= "All-Bal")
-async def All_Balances(ctx):
-    bal = list(1,2,3,4,5)
-    all_bal = {i:100 for i in bal}
-    await ctx.send(all_bal)
+@bot.event
+async def on_member_join(member):
+    with open("users.json", "r") as f:
+        users = json.load(f)
+
+    await update_data(users, member)
+
+    with open("users.json", "w") as f:
+        json.dump(users, f)
+
+@bot.event
+async def on_message(message):
+     with open("users.json", "r") as f:
+        users = json.load(f)
+
+    await update_data(users, message.author)
+
+    with open("users.json", "w") as f:
+        json.dump(users, f)
 
 @bot.command(name= "Coin-Flip") 
 async def Coin_Flip(ctx):
@@ -27,7 +47,7 @@ async def Coin_Flip(ctx):
         losses += 1
     
 @bot.command(name= "Emoji-Slot")
-async def emjoi_slot(ctx):
+async def emjoi_slot(ctx, users, user):
     import random
     options = ["🤡","😷","👽"]
     slot = [random.choice(options),random.choice(options),random.choice(options)]
@@ -37,10 +57,13 @@ async def emjoi_slot(ctx):
         global losses, wins
         losses += 1
         await ctx.send("Sorry son, this ain't it- ya lost some cash.")
-        await all_bal
+        await ctx.send(f"The new balance for {user.name} is {users[user.id]["balance"] - 10} ")
+        users[user.id]["balance"] = users[user.id]["balance"] - 10
     if outcome == True:
         wins += 1
         await ctx.send("Look at you! Don't be shy- play some more!")
+        await ctx.send(f"The new balance for {user.name} is {users[user.id]["balance"] + 10} ")
+        users[user.id]["balance"] = users[user.id]["balance"] + 10
 
 @bot.command(name = "Win:Loss")
 async def counter(ctx):
